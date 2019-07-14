18aaa0732945f0ddff8c25f0cd0df1f49eb3faa3
Alexander Zhidkov <azhidkov@gmail.com>
2019-07-14 12:14:26 +0800

    add business onboarding form

--- src/app/business-onboard/business-onboard.component.ts
+++ src/app/business-onboard/business-onboard.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./business-onboard.component.css']
 })
 export class BusinessOnboardComponent implements OnInit {
+  isLoading = false;
 
   constructor() { }
 
