7be4e4069ee430086aa30b882fbea94dcc075697
Alexander Zhidkov <azhidkov@gmail.com>
2019-07-28 11:13:57 +0800

    add businesses list component

---
+++ src/app/businesses/businesses.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FirestoreService } from '../services/firestore.service';
+
+@Component({
+  selector: 'app-businesses',
+  templateUrl: './businesses.component.html',
+  styleUrls: ['./businesses.component.css']
+})
+export class BusinessesComponent implements OnInit {
+  isLoading = true;
+  businesses: Observable<any[]>;
+
+  constructor(private firestore: FirestoreService<any>) { }
+
+  ngOnInit() {
+    this.firestore.setCollection('businesses');
+    this.businesses = this.firestore.list();
+    this.businesses.subscribe(e => {
+      this.isLoading = false;
+    });
+  }
+
+}
