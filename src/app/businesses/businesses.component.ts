70f3c1c897a151470f8955b1d03436e2645e32c2
Alexander Zhidkov <azhidkov@gmail.com>
2019-08-06 08:42:44 +0800

    filter out businesses by referrer

--- src/app/businesses/businesses.component.ts
+++ src/app/businesses/businesses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { FirestoreService } from '../services/firestore.service';
 
@@ -11,10 +12,18 @@ export class BusinessesComponent implements OnInit {
   isLoading = true;
   businesses: Observable<any[]>;
 
-  constructor(private firestore: FirestoreService<any>) { }
+  constructor(private route: ActivatedRoute, private firestore: FirestoreService<any>) { }
 
   ngOnInit() {
-    this.firestore.setCollection('businesses', ref => ref.where('isSubmitted', '==', true));
+    const urlSegments = this.route.snapshot.url;
+    if (urlSegments[0].path === 'referrer') {
+      this.firestore.setCollection('businesses', ref => ref
+        .where('isSubmitted', '==', true)
+        .where('createdBy.uid', '==', localStorage.getItem('uid')));
+    } else {
+      this.firestore.setCollection('businesses', ref => ref
+        .where('isSubmitted', '==', true));
+    }
     this.businesses = this.firestore.list();
     this.businesses.subscribe(e => {
       this.isLoading = false;
