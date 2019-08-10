52914c1a7ceb2f68206d689fed63b3989b51ee45
Alexander Zhidkov <azhidkov@gmail.com>
2019-08-10 15:03:19 +0800

    store app name in functions config

--- functions/index.js
+++ functions/index.js
@@ -37,7 +37,7 @@ const mailTransport = nodemailer.createTransport({
 
 // Your company name to include in the emails
 // TODO: Change this to your app or company name to customize the email sent.
-const APP_NAME = 'PracLearn';
+const APP_NAME = functions.config().app.name;
 
 /**
  * Sends an email for each new document in firestore collection.
