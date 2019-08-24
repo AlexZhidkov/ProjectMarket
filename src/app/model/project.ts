7a8b2d6f5f869465bd30ee02cea61c472282199f
Alexander Zhidkov <azhidkov@gmail.com>
2019-08-24 15:53:45 +0800

    add project status and description

--- src/app/model/project.ts
+++ src/app/model/project.ts
@@ -1,6 +1,8 @@
 import { NamedEntity } from './named-entity';
 
 export interface Project extends NamedEntity {
+    description: string;
+    status: string;
     business: NamedEntity;
     student: NamedEntity;
 }
