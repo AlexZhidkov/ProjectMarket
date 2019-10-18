b2220729a3500279f781f3eeb71bbd5df7baa05c
Alexander Zhidkov <azhidkov@gmail.com>
2019-10-18 18:46:25 +0800

    add app scripts

---
+++ google-apps-scripts/getBusinessesFromFirestore.js
@@ -0,0 +1,92 @@
+/**
+ * @OnlyCurrentDoc Limits the script to only accessing the current sheet.
+ */
+
+/**
+ * A special function that runs when the spreadsheet is open, used to add a
+ * custom menu to the spreadsheet.
+ */
+function onOpen() {
+    var spreadsheet = SpreadsheetApp.getActive();
+    var menuItems = [
+        { name: 'Import All Businesses', functionName: 'importBusinesses' }
+    ];
+    spreadsheet.addMenu('Experlio', menuItems);
+}
+
+function getBusinessesFromFirestore() {
+    var email = "";
+    var key = "";
+    var projectId = "";
+    var firestore = FirestoreApp.getFirestore(email, key, projectId);
+    const allBusinesses = firestore.getDocuments("businesses");
+    return allBusinesses;
+}
+
+function importBusinesses() {
+    var sheet = SpreadsheetApp.getActiveSheet().setName('Businesses');
+    sheet.clear();
+    var headers = [
+        'id',
+        'createdBy',
+        'abn',
+        'name',
+        'template',
+        'supervisor',
+        'address',
+        'fteNumber',
+        'additionalEmployees',
+        'isMentorAvailable',
+        'mentor',
+        'website',
+        'industry',
+        'description',
+        'startDate',
+        'endDate',
+        'supervisorRole',
+        'supervisorExperience',
+        'why - completeProject',
+        'why - testStudent',
+        'why - gainIdeas',
+        'why - developMentors',
+        'why - other',
+        'referrer',
+        'isSubmitted',
+        'submittedOn'
+    ];
+
+    sheet.getRange('A1:Z1').setValues([headers]).setFontWeight('bold');
+
+    var businesses = getBusinessesFromFirestore();
+    for each(var business in businesses)
+    {
+        sheet.appendRow([
+            business.name.split("/")[6],
+            business.fields.createdBy.name,
+            business.fields.abn,
+            business.fields.name,
+            business.fields.template,
+            business.fields.supervisor,
+            business.fields.address,
+            business.fields.fteNumber,
+            business.fields.additionalEmployees,
+            business.fields.isMentorAvailable,
+            business.fields.mentor,
+            business.fields.website,
+            business.fields.industry,
+            business.fields.description,
+            business.fields.startDate,
+            business.fields.endDate,
+            business.fields.supervisorRole,
+            business.fields.supervisorExperience,
+            business.fields.why.completeProject,
+            business.fields.why.testStudent,
+            business.fields.why.gainIdeas,
+            business.fields.why.developMentors,
+            business.fields.why.other,
+            business.fields.referrer,
+            business.fields.isSubmitted,
+            business.fields.submittedOn
+        ]);
+    };
+}
---
+++ google-apps-scripts/getUsersFromFirestore.js
@@ -0,0 +1,54 @@
+/**
+ * @OnlyCurrentDoc Limits the script to only accessing the current sheet.
+ */
+
+/**
+ * A special function that runs when the spreadsheet is open, used to add a
+ * custom menu to the spreadsheet.
+ */
+function onOpen() {
+    var spreadsheet = SpreadsheetApp.getActive();
+    var menuItems = [
+        { name: 'Import All Users', functionName: 'importUsers' }
+    ];
+    spreadsheet.addMenu('Experlio', menuItems);
+}
+
+function getUsersFromFirestore() {
+    var email = "";
+    var key = "";
+    var projectId = "";
+    var firestore = FirestoreApp.getFirestore(email, key, projectId);
+    const allUsers = firestore.getDocuments("users");
+    return allUsers;
+}
+
+function importUsers() {
+    var sheet = SpreadsheetApp.getActiveSheet().setName('Users');
+    sheet.clear();
+    var headers = [
+        'id',
+        'created',
+        'authEmail',
+        'authName',
+        'email',
+        'name',
+        'role'
+    ];
+
+    sheet.getRange('A1:G1').setValues([headers]).setFontWeight('bold');
+
+    var users = getUsersFromFirestore();
+    for each(var user in users)
+    {
+        sheet.appendRow([
+            user.name.split("/")[6],
+            user.createTime,
+            user.fields.authEmail,
+            user.fields.authName,
+            user.fields.email,
+            user.fields.name,
+            user.fields.role
+        ]);
+    };
+}
