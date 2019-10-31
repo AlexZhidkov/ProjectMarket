b0728facf41098cffc1b5c1b6472de3ed49c889f
Alexander Zhidkov <azhidkov@gmail.com>
2019-10-31 18:41:34 +0800

    Restict Referrer to see only relevant projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Project } from '../model/project';
+import { AuthService } from '../services/auth.service';
 import { FirestoreService } from '../services/firestore.service';
 
 @Component({
@@ -12,10 +14,20 @@ export class ProjectsComponent implements OnInit {
   isLoading = true;
   projects: Observable<Project[]>;
 
-  constructor(private firestore: FirestoreService<Project>) { }
+  constructor(
+    private firestore: FirestoreService<Project>,
+    private outlet: RouterOutlet,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
-    this.firestore.setCollection('projects');
+    if (this.outlet.activatedRouteData.isReferrer) {
+      this.firestore.setCollection('projects', ref => ref
+        .where('referrer.uid', '==', this.authService.currentUser().uid)
+      );
+    } else {
+      this.firestore.setCollection('projects');
+    }
     this.projects = this.firestore.list();
     this.projects.subscribe(() => {
       this.isLoading = false;
